5a42226caebad66b0a1b4a7eb3ef70e26c4d2e6e
Heytemisan <temyicy@gmail.com>
2022-01-19 09:33:07 +0100

    created Lesson1 folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Greet } from './components/Lesson1/Greet';
 
 function App() {
   return (
     <div className="App">
-      
+      <Greet/>
     </div>
   );
 }
