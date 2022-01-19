78b25c0d5a0449c2ccc39b76305d97d74f50db88
Heytemisan <temyicy@gmail.com>
2022-01-19 21:46:06 +0100

    intialized

--- src/components/06Lesson-Event/Input.tsx
+++ src/components/06Lesson-Event/Input.tsx
@@ -1,7 +1,12 @@
-//input value usually requires two props
-//inputValue and event handler
+//input value usually requires two props -
+//input value and event handler
+type InputProps = {
+    value: string,
+    handleChange:(event:React.ChangeEvent<HTMLInputElement>) => void
+     //usually input doesn not require a parameter
+}
 
-export const Input =()=> {
+export const Input =(props: InputProps)=> {
     return (
         <input type='text'/>
     )
