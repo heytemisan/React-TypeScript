e701ffa2ef0d8ca583c1abe06f3540323ccb4842
Heytemisan <temyicy@gmail.com>
2022-01-19 13:55:16 +0100

    initialized

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Greet } from './components/01Lesson-Greetings/Greet';
 import { Person } from './components/02Lesson2-Person/Person';
 import { PersonList } from './components/03Lesson-Personlist/Personlist';
 import { Status } from './components/04Lesson-status/Status';
+import { Heading } from './components/05-Lesson-childrenProps/Heading';
 
 function App() {
 
@@ -35,6 +36,7 @@ function App() {
       <Person name={personName}/>
       <PersonList names={nameList}/>
       <Status status='loading'/>
+      <Heading>Child props</Heading>
     </div> 
   );
 }
---
+++ src/components/05-Lesson-childrenProps/Heading.tsx
@@ -0,0 +1,12 @@
+type headingProps = {
+    children:string
+}
+
+export const Heading =(props: headingProps) => {
+    return (
+        <h2>
+            {props.children} 
+        </h2>
+        
+    )
+}
