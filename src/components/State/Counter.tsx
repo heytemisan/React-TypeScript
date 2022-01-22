f2a63b59ca90f3e601b1ae7568474118485ccea7
Heytemisan <temyicy@gmail.com>
2022-01-22 16:30:50 +0100

    fix:modified using descriminated unions

--- src/components/State/Counter.tsx
+++ src/components/State/Counter.tsx
@@ -10,10 +10,25 @@ type CounterState = {
     count:number
 }
 
-type CounterAction = {
-    type: 'increment' | 'decrement'
+//descriminated unions
+
+type UpdateAction = {
+    type:'increment' | 'decrement'
     payload:number
 }
+
+type ResetAction = {
+    type:'reset'
+}
+
+
+
+type CounterAction = UpdateAction | ResetAction;
+
+// type CounterAction = {
+//     type: 'increment' | 'decrement' | 'reset'
+//     payload?:number
+// }
 
 function reducer(state: CounterState, action: CounterAction) {
     //action type update count value 
@@ -22,6 +37,8 @@ function reducer(state: CounterState, action: CounterAction) {
             return {count: state.count + action.payload}
         case 'decrement':
             return {count: state.count - action.payload}
+        case 'reset':
+            return initialState;
         default:
             return state
     }
@@ -34,6 +51,7 @@ export const Counter = () => {
             Count:{state.count}
             <button onClick={()=> dispatch({type: 'increment', payload:10})}>increment 10</button>
             <button onClick={()=> dispatch({type: 'decrement', payload:10})}>decrement 10</button>
+            <button onClick={()=> dispatch({type: 'reset'})}>reset</button>
         </>
     )
 }
